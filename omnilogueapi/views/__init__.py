3078e93e248b3d0c8acf6ae62b731691eaf6dba9
Nate Z <ntzeitlin@gmail.com>
2025-04-24 15:47:51 -0500

    initialized category viewset

--- omnilogueapi/views/__init__.py
+++ omnilogueapi/views/__init__.py
@@ -1,1 +1,2 @@
 from .users import UserViewSet
+from .stories import StoryViewSet
---
+++ omnilogueapi/views/categories.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponseServerError
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+from omnilogueapi.models import Category
+
+class CategoryViewSet(ViewSet):
+    pass
+
+class CategorySerializer(serializers.ModelSerializer):
