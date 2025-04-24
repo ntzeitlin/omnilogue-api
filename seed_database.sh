09487ee1ce068228f786bda338406daeb8c6a414
Nate Z <ntzeitlin@gmail.com>
2025-04-24 13:45:53 -0500

    all fixtures created and seeded

--- seed_database.sh
+++ seed_database.sh
@@ -8,3 +8,12 @@ python3 manage.py migrate omnilogueapi
 python3 manage.py loaddata users
 python3 manage.py loaddata tokens
 python3 manage.py loaddata userprofiles
+python3 manage.py loaddata categories
+python3 manage.py loaddata tags
+python3 manage.py loaddata stories
+python3 manage.py loaddata story_tags
+python3 manage.py loaddata story_sections
+python3 manage.py loaddata story_links
+python3 manage.py loaddata bookshelves
+python3 manage.py loaddata reviews
+python3 manage.py loaddata story_reading_histories
