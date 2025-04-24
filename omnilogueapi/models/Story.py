5698195a1cbc362690ed23f8d95b0ad7ccd262a7
Nate Z <ntzeitlin@gmail.com>
2025-04-24 11:51:23 -0500

    Story Model with all fields

---
+++ omnilogueapi/models/Story.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Story(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="stories_created"
+    )
+    title = models.CharField(max_length=255, unique=True)
+    subtitle = models.TextField()
+    description = models.TextField()
+    excerpt = models.TextField()
+    is_public = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)  # Add a timestamp on creation
+    updated_at = models.DateTimeField(auto_now=True)  # Add a timestamp on each save
+    category = models.ForeignKey(
+        "Category", on_delete=models.CASCADE, related_name="stories_in_category"
+    )
+    is_interactive = models.BooleanField(default=False)
