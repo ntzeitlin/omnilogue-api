e3e10105e2ffee1e954aed16e809eb04f2e9623c
Nate Z <ntzeitlin@gmail.com>
2025-04-28 14:45:25 -0500

    added signaling so that when a new User registers, API will auto generate UserProfile and Bookshelf tables for the User

--- omnilogueapi/models/user_profile.py
+++ omnilogueapi/models/user_profile.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
@@ -9,3 +11,9 @@ class UserProfile(models.Model):
     website = models.TextField()
     display_name = models.TextField()
     location = models.TextField()
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
