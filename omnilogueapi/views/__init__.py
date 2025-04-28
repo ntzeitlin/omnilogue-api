094041cbfae8ad445cdfefdfe47a9632e0c25711
Nate Z <ntzeitlin@gmail.com>
2025-04-28 14:32:54 -0500

    client can now GET to /bookshelves/ to receive current user's list of shelved books

--- omnilogueapi/views/__init__.py
+++ omnilogueapi/views/__init__.py
@@ -1,2 +1,3 @@
 from .users import UserViewSet
 from .stories import StoryViewSet
+from .bookshelf import BookshelfViewSet
--- omnilogueapi/views/bookshelf.py
+++ omnilogueapi/views/bookshelf.py
@@ -2,8 +2,8 @@ from django.http import HttpResponseServerError
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
-from omnilogueapi.models import Story
-from .users import UserSerializer
+from omnilogueapi.models import Story, UserProfile, BookshelfStory
+from .stories import StoryOverviewSerializer
 
 # ViewSet to handle GET, PUT, DELETE from bookshelf
 # Bookshelf View
@@ -14,4 +14,17 @@ from .users import UserSerializer
 
 
 class BookshelfViewSet(ViewSet):
-    pass
+    def list(self, request):
+        current_user = UserProfile.objects.get(user=request.auth.user)
+        bookshelf_stories = current_user.user.bookshelf.stories
+        serializer = BookshelfSerializer(bookshelf_stories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# SERIALIZERS
+class BookshelfSerializer(serializers.ModelSerializer):
+    story = StoryOverviewSerializer(many=False)
+
+    class Meta:
+        model = BookshelfStory
+        fields = ("id", "story")
