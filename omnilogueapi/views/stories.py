7dba3760783dd81f1158a0d56b7110b1ea9a5659
Nate Z <ntzeitlin@gmail.com>
2025-04-30 21:14:54 -0500

    enabled multi-sectional content processing

--- omnilogueapi/views/stories.py
+++ omnilogueapi/views/stories.py
@@ -47,26 +47,26 @@ class StoryViewSet(ViewSet):
 
         # content should be an array, for each section in the content array, generate a new story section
 
-        StorySection.objects.create(
-            story=story,
-            title=story.title,
-            content=request.data["content"],
-            order=1,
-            file_path="",
-        )
+        # StorySection.objects.create(
+        #     story=story,
+        #     title=story.title,
+        #     content=request.data["content"],
+        #     order=1,
+        #     file_path="",
+        # )
 
-        # story_content = request.data["content"]
-        # print(story_content)
-        # count = 0
-        # for section in story_content:
-        #     count = count + 1
-        #     StorySection.objects.create(
-        #         story=story,
-        #         title=section["title"],
-        #         content=section["content"],
-        #         order=count,
-        #         file_path="",
-        #     )
+        story_content = request.data["content"]
+        print(story_content)
+        count = 0
+        for section in story_content:
+            count = count + 1
+            StorySection.objects.create(
+                story=story,
+                title=section["title"],
+                content=section["content"],
+                order=count,
+                file_path="",
+            )
 
         try:
             serializer = StoryDetailSerializer(story, many=False)
